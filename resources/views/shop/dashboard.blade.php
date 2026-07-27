3c17e0ffa0e9670d6458e58e3e68048e39ef5a34
Chandan Gaur <chandangaurjecrc@gmail.com>
2026-07-27 20:10:21 +0530

    fix(dashboard): Correct Order Analytics route links to shop.order.index on Shop Dashboard

--- resources/views/shop/dashboard.blade.php
+++ resources/views/shop/dashboard.blade.php
@@ -121,7 +121,7 @@
                 @endphp
                 <div class="d-flex flex-wrap gap-3 orderStatus">
                     @foreach ($orderStatuses as $status)
-                        <a href="{{ route('admin.order.index', str_replace(' ', '_', $status->value)) }}"
+                        <a href="{{ route('shop.order.index', str_replace(' ', '_', $status->value)) }}"
                             class="d-flex status flex-grow-1 {{ Str::camel($status->value) }}">
                             <div class="d-flex align-items-center gap-2 justify-content-between w-100">
                                 <div class="d-flex align-items-center gap-2">
