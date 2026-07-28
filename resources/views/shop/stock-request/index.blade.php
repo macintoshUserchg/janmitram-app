c78ddc1e92a4aa81b2c635ae5da961e9a14d6911
Chandan Gaur <chandangaurjecrc@gmail.com>
2026-07-28 19:38:37 +0530

    fix(stock-request): Fix 500 ViewException error by extending layouts.app across all stock-request Blade views

--- resources/views/shop/stock-request/index.blade.php
+++ resources/views/shop/stock-request/index.blade.php
@@ -1,4 +1,4 @@
-@extends('shop.layouts.app')
+@extends('layouts.app')
 
 @section('title', __('Warehouse Stock Requests'))
 
--- resources/views/shop/stock-request/show.blade.php
+++ resources/views/shop/stock-request/show.blade.php
@@ -1,4 +1,4 @@
-@extends('shop.layouts.app')
+@extends('layouts.app')
 
 @section('title', __('Stock Request Detail'))
 
